ca4e3769c67e85e28ddf60aee9d3f4718e1e6794
hoangtuan2k5 <hoangchieunguyentuan@gmail.com>
2025-01-14 14:08:36 +0700

    remove default api key

--- src/config.py
+++ src/config.py
@@ -3,9 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GROQ_API_KEY = os.getenv(
-    "GROQ_API_KEY", "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"
-)
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 API_URL = "https://api.groq.com/openai/v1/chat/completions"
 MODEL = "llama-3.3-70b-versatile"
 TEMPERATURE = 0.75
